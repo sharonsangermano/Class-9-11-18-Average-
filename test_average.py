b977aa996d8e8f4c2cc0aa3d7a11f776c103c9f2
Sharon Sangermano <sharon.sangermano@duke.edu>
2018-09-24 14:12:27 -0400

    edited for pep8 formating

--- test_average.py
+++ test_average.py
@@ -1,14 +1,10 @@
-def test_add_three(): 
+def test_add_three():
     from average import add_three
-    s = add_three(4,5,6)
+    s = add_three(4, 5, 6)
     assert s == 15
 
 
 def test_divide_three():
     from average import divide_three
-    a = divide_three(15,3)
+    a = divide_three(15, 3)
     assert a == 5
-
-
-
-
